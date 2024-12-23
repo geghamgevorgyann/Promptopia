3c88af9a0026d8fb4c4ebf5836af0efd16dd7c4b
geghamgevorgyann <gegham.gevorgyan.2003@mail.ru>
2024-12-23 16:14:09 +0400

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,21 @@
 "use client"
 import Feed from "@components/Feed";
-import {useEffect} from "react";
+// import {useEffect} from "react";
 
 
 const Home = () => {
-    useEffect(() => {
-        if ('serviceWorker' in navigator) {
-            navigator.serviceWorker
-                .register('/service-worker.js')
-                .then((registration) => {
-                    console.log('Service Worker registered with scope:', registration.scope);
-                })
-                .catch((error) => {
-                    console.error('Service Worker registration failed:', error);
-                });
-        }
-    }, []);
+    // useEffect(() => {
+    //     if ('serviceWorker' in navigator) {
+    //         navigator.serviceWorker
+    //             .register('/service-worker.js')
+    //             .then((registration) => {
+    //                 console.log('Service Worker registered with scope:', registration.scope);
+    //             })
+    //             .catch((error) => {
+    //                 console.error('Service Worker registration failed:', error);
+    //             });
+    //     }
+    // }, []);
   return (
     <section className="w-full flex-center flex-col">
       <h1 className="head_text text-center">
